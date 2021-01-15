295dbc94abdba292a0b44e217c535a20369a7542
illia_repiah <illia.iepiakh@trackimo.com>
2021-01-15 19:17:19 +0200

    SMXS-14: Added alarm types from spec revision

--- src/dto/alarms_dto.go
+++ src/dto/alarms_dto.go
@@ -1,5 +1,7 @@
 package dto
 
+import "reflect"
+
 type ChannelNumberAlarmParameter struct {
 	Channel     uint `json:"CHANNEL,omitempty"`     // position of bit is considered a channel number starts from 1
 	ChannelMask uint `json:"CHANNELMASK,omitempty"` // position of bit is considered a channel alarm valid starts from 1
@@ -174,4 +176,422 @@ type SpeedAlarmParameter struct {
 type LowVoltageAlarmParameter struct {
 	V    int  // 1/100 Volt
 	PUSH uint // 0, 1
+}
+
+type SerialAlarmParameter struct {
+	ALARMNAME string   //Indicates the name of the alarm, 32 bytes
+	SER       string   //Abbreviation of the name of the alarm
+	LCH       uint     //The channel number, by bit address
+	SUM       int      //Total number of serial alarms
+	S         []string //Serial alarm information description
+}
+
+type direction byte
+
+var DirectionEnum = struct {
+	XDirection        direction
+	YDirection        direction
+	ZDirection        direction
+	Collision         direction
+	RollOver          direction
+	Bumps             direction
+	HarshAcceleration direction
+	HardBrake         direction
+	G4                direction
+	G5                direction
+	HarshLeftTurn     direction
+	HarshRightTurn    direction
+}{
+	XDirection:        0,
+	YDirection:        1,
+	ZDirection:        2,
+	Collision:         3,
+	RollOver:          4,
+	Bumps:             5,
+	HarshAcceleration: 6,
+	HardBrake:         7,
+	G4:                8,
+	G5:                9,
+	HarshLeftTurn:     10,
+	HarshRightTurn:    11,
+}
+
+type AccAlarmParameter struct {
+	ALARMNAME string //Indicates the name of the alarm, 32 bytes
+	SER       string //Abbreviation of the name of the alarm
+	LCH       uint   //The channel number, by bit address
+	D         direction
+	X         int // *1000
+	Y         int
+	Z         int
+	V         int //G4, G5 effective alarm
+}
+
+type fenceEvent byte
+
+var FenceEventEnum = struct {
+	IntoFence            fenceEvent
+	OutOfFence           fenceEvent
+	IncomingLine         fenceEvent
+	OutOfLine            fenceEvent
+	LineDeviation        fenceEvent
+	DrivingTimeIsTooLong fenceEvent
+}{
+	IntoFence:            1 << 0,
+	OutOfFence:           1 << 1,
+	IncomingLine:         1 << 2,
+	OutOfLine:            1 << 3,
+	LineDeviation:        1 << 4,
+	DrivingTimeIsTooLong: 1 << 5,
+}
+
+type fenceType byte
+
+var fenceTypeEnum = struct {
+	round     fenceType
+	rectangle fenceType
+	polygon   fenceType
+	line      fenceType
+}{
+	round:     1,
+	rectangle: 2,
+	polygon:   3,
+	line:      4,
+}
+
+type GeoFenceAlarmParameter struct {
+	E fenceEvent
+
+	//Out of the fence action
+	//N represents the subscript of the fence event type bit,
+	// AC[N] uint
+	AT fenceType
+	ID int    //fence ID
+	AN string //fence name
+}
+
+type stopAnnouncement byte
+
+var stopAnnouncementEnum = struct {
+	RetentionInStop         stopAnnouncement
+	PassStopWithoutStopping stopAnnouncement
+}{
+	RetentionInStop:         0,
+	PassStopWithoutStopping: 1,
+}
+
+type PeripheralDroppedAlarmParameter struct {
+	AN  string // name
+	SER string //Abbreviation of the name of the alarm
+	ID  int    //Peripheral ID
+}
+
+type StopAnnouncementAlarmParameter struct {
+	AN  string           // name
+	SER string           //Abbreviation of the name of the alarm
+	S   stopAnnouncement //Stop announcement alarm subtype
+}
+
+type FatigueDrivingSubtype byte
+
+var FatigueDrivingSubtypeEnum = struct {
+	FatigueAlarm                FatigueDrivingSubtype
+	FatiguePreAlarm             FatigueDrivingSubtype
+	DailyCumulativeFatigueAlarm FatigueDrivingSubtype
+}{
+	FatigueAlarm:                0,
+	FatiguePreAlarm:             1,
+	DailyCumulativeFatigueAlarm: 2,
+}
+
+type FatigueDrivingAlarmParameter struct {
+	AN string //name
+	AS int    //Fatigue driving subtype
+	C  int    //Current driving duration length in seconds
+	D  uint   //Fatigue level, 0-100, use non-negative integer to represent
+}
+
+type TimeoutParkingAlarmParameter struct {
+	AN string //name
+	C  int    //The length of the current parking duration, in seconds
+}
+
+type GestureAlarmType byte
+
+var GestureAlarmTypeEnum = struct {
+	Brakes                             GestureAlarmType
+	AnxiousToSpeedUp                   GestureAlarmType
+	SharpTurn                          GestureAlarmType
+	NeutralGlide                       GestureAlarmType
+	EngineOverturned                   GestureAlarmType
+	IdleTimeIsTooLong                  GestureAlarmType
+	LaneDeviationLeftLine              GestureAlarmType
+	ForwardCollision                   GestureAlarmType
+	CarDistanceTooClose                GestureAlarmType
+	Rollover                           GestureAlarmType
+	Collision                          GestureAlarmType
+	LaneDepartureFromPreAlarmRightLine GestureAlarmType
+	VideoOcclusion                     GestureAlarmType
+	FatigueAlarmFromPeripherals        GestureAlarmType
+	AntiCollisionFromPeripherals       GestureAlarmType
+	PhoneCallMakingFromPeripherals     GestureAlarmType
+	SmokingFromPeripherals             GestureAlarmType
+	WatchingTheFront                   GestureAlarmType
+	SilentHelpRequest                  GestureAlarmType
+}{
+	Brakes:                             0,
+	AnxiousToSpeedUp:                   1,
+	SharpTurn:                          2,
+	NeutralGlide:                       3,
+	EngineOverturned:                   4,
+	IdleTimeIsTooLong:                  5,
+	LaneDeviationLeftLine:              6,
+	ForwardCollision:                   7,
+	CarDistanceTooClose:                8,
+	Rollover:                           9,
+	Collision:                          10,
+	LaneDepartureFromPreAlarmRightLine: 11,
+	VideoOcclusion:                     12,
+	FatigueAlarmFromPeripherals:        13,
+	AntiCollisionFromPeripherals:       14,
+	PhoneCallMakingFromPeripherals:     15,
+	SmokingFromPeripherals:             16,
+	WatchingTheFront:                   17,
+	SilentHelpRequest:                  18,
+}
+
+type GestureDataSRC string
+
+var GestureDataSRCEnum = struct {
+	CanInfo   GestureDataSRC
+	SixAxis   GestureDataSRC
+	TirePress GestureDataSRC
+	TruckLoad GestureDataSRC
+}{
+	CanInfo:   "CANINFO（OBD）",
+	SixAxis:   "6-AXIS",
+	TirePress: "TIREPRESS",
+	TruckLoad: "TRUCKLOAD",
+}
+
+var GestureDataSRCMap = map[string]reflect.Type{
+	string(GestureDataSRCEnum.CanInfo):   reflect.TypeOf(OBDOriginalStatusParameter{}),
+	string(GestureDataSRCEnum.SixAxis):   reflect.TypeOf(AXISOriginalStatusParameter{}),
+	string(GestureDataSRCEnum.TirePress): reflect.TypeOf(TyrePressureOriginalStatusParameter{}),
+	string(GestureDataSRCEnum.TruckLoad): reflect.TypeOf(LoadOriginalStatusParameter{}),
+}
+
+type GestureAlarmParameter struct {
+	ALARMNAME string
+	SER       string // abbreviation
+	LCH       uint   //The channel number of the linked video, bit representation
+	S         GestureAlarmType
+	DATSRC    GestureDataSRC
+	DATA      interface{}
+}
+
+type OBDOriginalStatusParameter struct {
+	REFVALUE    int     //Threshold
+	RPM         int     //Engine RPM
+	COOLANTTEMP int     //Coolant temperature
+	OILPRESSURE float32 //Engine oil pressure
+	KEYSTATUS   bool    //Car key switch
+	ACCEL       int     //The accelerator pedal depresses the percentage
+	SPEED       int     //Speed
+	BATTERY     float32 //Battery voltage
+	OILWEAR     float32 //Total fuel consumption
+	OILTEMP     float32 //Engine oil temperature
+	ENGINENUM   string  //Engine number
+}
+
+type AXISOriginalStatusParameter struct {
+	REFVALUE         int     //Threshold values to trigger an event.
+	DATSRC           string  //Raw data source
+	ACCELX           float32 //Acceleration in X axis. Unit:G.；
+	ACCELY           float32 //Acceleration in Y axis. Unit:G.
+	ACCELZ           float32 //Acceleration in Z axis. Unit:G.
+	ANGULARVELOCITYX float32 //Rotational speed in X axis. Unit:degree/second.
+	ANGULARVELOCITYY float32 //Rotational speed in Y axis. Unit:degree/second.
+	ANGULARVELOCITYZ float32 //Rotational speed in Z axis. Unit:degree/second.
+}
+
+type TyrePressureOriginalStatusParameter struct {
+	REFVALUE string //Threshold values to trigger an event.
+	DATSRC   string //Raw data source
+	TNUM     uint   //The quantity of tire
+	TIRE     []uint //Tire data array
+}
+
+type TyreParameter struct {
+	ID    string  //Serial ID
+	NAME  string  //Name
+	PRESS float32 //Pressure (float)
+	TEMP  float32 //Temperature (float)
+}
+
+type LoadOriginalStatusParameter struct {
+	REFVALUE  string //Threshold values to trigger an event.
+	DATSRC    string //Raw data source
+	AXLESNUM  int    //Number of axles
+	LOADLIMIT int    //Load limit
+	LEFT      int    //Left load
+	RIGHT     int    //Right load
+	TOTAL     int    //Total load
+}
+
+type GreenDrivingAlarmParameter struct {
+	DGSL GreenDrivingDataParameter
+}
+
+type GreenDrivingDataParameter struct {
+	B []interface{}
+}
+
+type CustomExternalInputAlarm struct {
+	SNO       byte //alarm id
+	ALARMNAME string
+	SER       string //Abbreviation of the name of the alarm
+	//SNO indicates valid alarm IO number, starting from 1, and each LCH element
+	//represents the bit set corresponding to the linkage channel number associated
+	//with the Nth IO alarm, this set is a bit set. For example: IO1 occurs motion
+	//detection alarm, linked to video from channel 0, channel 1, channel 2,
+	//means LCH [1] = 15 (decimal).
+	//LCH[N] uint
+	PUSH byte // 1 need to push, 0 don't
+	FROM uint
+	SUB  CustomExternalInputAlarmSubtype
+}
+
+type CustomExternalInputAlarmSubtype byte
+
+var CustomExternalInputAlarmSubtypeEnum = struct {
+	NoAlarm           CustomExternalInputAlarmSubtype
+	AlarmOccurred     CustomExternalInputAlarmSubtype
+	TrimmingOccurs    CustomExternalInputAlarmSubtype
+	ShortCircuitAlarm CustomExternalInputAlarmSubtype
+}{
+	NoAlarm:           0,
+	AlarmOccurred:     1,
+	TrimmingOccurs:    2,
+	ShortCircuitAlarm: 3,
+}
+
+type BusLaneOccupationAlarmParameter struct {
+	F         byte //Bus lane occupation detection type 0: to IPC intelligent module active detection.
+	ALARMNAME string
+	SER       string //Abbreviation of the name of the alarm
+	PUSH      byte   // 1 need to push, 0 don't
+	NO        []interface{}
+	C         []interface{}
+}
+
+type OilVolumeAlarmParameter struct {
+	ALARMNAME string //Indicates the name of the alarm, 32 bytes
+	SER       string //Abbreviation of the name of the alarm
+	LCH       byte   //he channel number of the linked video, bit represent
+}
+
+type SpecialCustomerMalfunctionAlarmParameter struct {
+	AS SpecialCustomerMalfunctionType
+}
+
+type SpecialCustomerMalfunctionType byte
+
+var SpecialCustomerMalfunctionTypeEnum = struct {
+	LvLowVoltageMalfunction      SpecialCustomerMalfunctionType
+	HvHighVoltageMalfunction     SpecialCustomerMalfunctionType
+	HtHighTemperatureMalfunction SpecialCustomerMalfunctionType
+	HeaterFailure                SpecialCustomerMalfunctionType
+	WatchdogReset                SpecialCustomerMalfunctionType
+	OverCurrent                  SpecialCustomerMalfunctionType
+	TimeInvalid                  SpecialCustomerMalfunctionType
+	SDFull                       SpecialCustomerMalfunctionType
+	USBFull                      SpecialCustomerMalfunctionType
+	FirmwareUpdate               SpecialCustomerMalfunctionType
+	ConfigLoad                   SpecialCustomerMalfunctionType
+	TimeJumpMoreThan24Hours      SpecialCustomerMalfunctionType
+}{
+	LvLowVoltageMalfunction:      0,
+	HvHighVoltageMalfunction:     1,
+	HtHighTemperatureMalfunction: 2,
+	HeaterFailure:                3,
+	WatchdogReset:                4,
+	OverCurrent:                  5,
+	TimeInvalid:                  6,
+	SDFull:                       7,
+	USBFull:                      8,
+	FirmwareUpdate:               9,
+	ConfigLoad:                   10,
+	TimeJumpMoreThan24Hours:      11,
+}
+
+type TemperatureAbnormallyAlarmParameter struct { //3.4.1.5.27
+	ALARMNAME string
+	S         byte //0 high temperature, 1 low temperature
+	CU        int  //Current temperature
+	UP        int  //High temperature range
+	LOW       int  //Low temperature range
+	ID        int  //Sensor ID
+	TH        int  //Alarm threshold
+	TN        int  //Number of sensors
+}
+
+type AbnormalTemperatureChangeAlarmParameter struct {
+	ALARMNAME string
+	SER       string //Abbreviation of the name of the alarm
+	LCH       byte   //channel number of the linked video, bit represent
+	TU        int    //Temperature unit 0: (1/100) degrees Celsius 1: (1/100) degrees Fahrenheit
+	T         int    //Temperature value
+	AT        fenceType
+	ID        int    //Fence ID
+	AN        string //fence name
+	SID       int    //The ID of the sensor, starts from 1
+}
+
+type SmokeAlarmParameter struct {
+	ALARMNAME string
+	SER       string //Abbreviation of the name of the alarm
+	LCH       byte   //channel number of the linked video, bit represent
+	SDU       int    //Smoke concentration unit 0：(1/10000)mg/m3 ; 1：(1/100)ppb
+	SD        int    //Smoke concentration value
+	AT        fenceType
+	ID        int    //Fence ID
+	AN        string //fence name
+	SID       int    //The ID of the sensor, starts from 1
+}
+
+type GBoxAlarmParameter struct {
+	AS GBoxASType
+}
+
+type GBoxASType byte
+
+var GBoxASTypeEnum = struct {
+	RapidDeceleration                        GBoxASType
+	RapidAcceleration                        GBoxASType
+	OverSpeed                                GBoxASType
+	FatigueEvent                             GBoxASType
+	EngineOverturned                         GBoxASType
+	UseAirConditionerWhenIdling              GBoxASType
+	NeutralSliding                           GBoxASType
+	TooLongIdle                              GBoxASType
+	ColdStart                                GBoxASType
+	OpenTheDoorWhenVehicleIsMovingFrontDoor  GBoxASType
+	OpenTheDoorWhenVehicleIsMovingMiddleDoor GBoxASType
+}{
+	RapidDeceleration:                        1,
+	RapidAcceleration:                        2,
+	OverSpeed:                                3,
+	FatigueEvent:                             4,
+	EngineOverturned:                         5,
+	UseAirConditionerWhenIdling:              6,
+	NeutralSliding:                           7,
+	TooLongIdle:                              8,
+	ColdStart:                                9,
+	OpenTheDoorWhenVehicleIsMovingFrontDoor:  10,
+	OpenTheDoorWhenVehicleIsMovingMiddleDoor: 11,
+}
+
+type LicensePlateRecognitionAlarm struct {
+	//CAR[N] string //Identified license plate number,
 }
