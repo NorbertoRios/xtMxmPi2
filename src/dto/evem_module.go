cb2243a0b613733e140de20b3aebafb08442e4ce
illia_repiah <illia.iepiakh@trackimo.com>
2021-01-27 14:55:15 +0200

    CMDTYPE description added

--- src/dto/evem_module.go
+++ src/dto/evem_module.go
@@ -82,11 +82,14 @@ type EvemResponse struct {
 }
 
 type GeneralParamPayload struct {
-	ALARMUID  int //Important distinctive counter, +1 for each new alarm, unique by alarm
+	ALARMUID int //Important distinctive counter, +1 for each new alarm, unique by alarm
+	//0- Disable alarm, 1--Start alarm, 2-- Pre Alarm.   3-Continuous alarm
+	//camera itself changing states, long alarms use CMDTYPE 1 and then same data with 0 and CMDNO +1
+	//or timeless alarms use CMDTYPE 1 with no repeats
 	CMDTYPE   int
 	RUN       int
 	ALARMTYPE AlarmType
-	CMDNO     int
+	CMDNO     int //increasing in each command (start alarm or disable alarm via CMDTYPE)
 }
 
 func (e Evem) createResponse(eP *ActualDSMAlarmParameter) Evem {
