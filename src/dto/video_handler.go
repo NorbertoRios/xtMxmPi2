6b1417fcd6f68df8f2d50dd62a45df66bd5d787d
illia_repiah <illia.iepiakh@trackimo.com>
2021-03-16 10:27:56 +0200

    SMXS-31: added fps counter logic

--- src/dto/video_handler.go
+++ src/dto/video_handler.go
@@ -9,7 +9,10 @@ import (
 
 type VideoHandler struct {
 	videoFile       *os.File
-	offset          int64
+	audioFile       *os.File
+	rawFile         *os.File
+	frameCounter    int
+	frameMap        map[int]int
 	containerBuffer *TSOBuffer
 }
 
@@ -24,6 +27,7 @@ func (v VideoHandlerModule) HandleRequest(c interfaces.IChannel, buffer []byte) 
 		c.SetVideoHandler(vh)
 	}
 	handler = c.GetVideoHandler().(*VideoHandler)
+	handler.rawFile.Write(buffer)
 	segmentBuffer := HandlePackageWithSO(handler.containerBuffer, buffer[12:], c, &videoContainer.VideoFrameHeader{}, handleNormalizedPayload)
 	for segmentBuffer != nil {
 		segmentBuffer = HandlePackageWithSO(handler.containerBuffer, segmentBuffer, c, &videoContainer.VideoFrameHeader{}, handleNormalizedPayload)
@@ -34,10 +38,34 @@ func handleNormalizedPayload(c interfaces.IChannel, buffer []byte) {
 	videoHandler := c.GetVideoHandler().(*VideoHandler)
 	frames := videoContainer.ParseIntegerFrames(buffer[:])
 	for _, fr := range frames {
-		if fr.Header.FrameType == videoContainer.H264I || fr.Header.FrameType == videoContainer.H264P {
+		if fr.Header.FrameType == videoContainer.Audio {
+			videoHandler.audioFile.Write(fr.Data[4:])
+		}
+		if fr.Header.FrameType == videoContainer.H264I {
+			fc := videoHandler.frameCounter
+			if _, found := videoHandler.frameMap[fc]; found {
+				videoHandler.frameMap[fc] = fc + 1
+			} else {
+				videoHandler.frameMap[fc] = 1
+			}
+			videoHandler.frameCounter = 1
+			videoHandler.videoFile.Write(fr.Data)
+		}
+		if fr.Header.FrameType == videoContainer.H264P {
+			videoHandler.frameCounter++
 			videoHandler.videoFile.Write(fr.Data)
 		}
 	}
+}
+
+func (v VideoHandler) GetFrameMedian() int {
+	cK, maxV := 0, 0
+	for k, v := range v.frameMap {
+		if maxV < v {
+			cK, maxV = k, v
+		}
+	}
+	return cK
 }
 
 func (v VideoHandlerModule) ParseDtoFromData(buffer []byte) interface{} {
@@ -55,8 +83,11 @@ func createFile(name string) *os.File {
 
 func createVideoHandler() *VideoHandler {
 	return &VideoHandler{
-		videoFile:       createFile("server_file_04.03"),
-		offset:          0,
+		videoFile:       createFile("server_file_16.03_g711a_lq.video"),
+		audioFile:       createFile("server_file_16.03_g711a_lq.audio"),
+		rawFile:         createFile("server_file_16.03_g711a_lq.h264"),
+		frameCounter:    0,
 		containerBuffer: &TSOBuffer{},
+		frameMap:        make(map[int]int),
 	}
 }
